1b0f6fb294b8f109375e6a72e119376985745831
shangwu yao <shangwuyao@gmail.com>
2017-03-30 10:36:47 +0000

    SendGrid official guide

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,6 +6,21 @@ class ContactMailer < ActionMailer::Base
         @email = email
         @body = body
         
-        mail(from: email, subject: 'Contact From Message')
+        require 'sendgrid-ruby'
+        include SendGrid
+        
+        from = @email
+        to = Email.new(email: 'awp135@126.com')
+        subject = 'Sending with SendGrid is Fun'
+        content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+        mail = Mail.new(from, subject, to, content)
+        
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        response = sg.client.mail._('send').post(request_body: mail.to_json)
+        puts response.status_code
+        puts response.body
+        puts response.headers
+        
+        #mail(from: email, subject: 'Contact From Message')
     end
 end
